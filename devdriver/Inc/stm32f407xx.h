041e991c90e3d5ac71de8a926e8eb2c74c0ad8f2
Metehan Kemal Kayaalp <metehankemalkayaalp@gmail.com>
2024-04-30 21:54:33 +0300

    Update stm32f407xx.h
    defined some base address

--- devdriver/Inc/stm32f407xx.h
+++ devdriver/Inc/stm32f407xx.h
@@ -8,6 +8,22 @@
 #ifndef INC_STM32F407XX_H_
 #define INC_STM32F407XX_H_
 
+/*
+ * Memory Base Address (Address starting hex number on reference manual)
+ *
+ */
+
+#define FLASH_BASE_ADDR  (0x08000000UL) /* Defined FLASH base address(up to 1 MB)  */
+
+#define SRAM1_BASE_ADDR  (0x20000000UL) /* Defined SRAM1 base address(up to 112 KB)*/
+#define SRAM2_BASE_ADDR  (0x2001C000UL) /* Defined SRAM2 base address(up to 16 KB) */
+
+/*
+ * Peripheral Base Addresses (Address starting hex number on reference manual)
+ *
+ */
+
+#define PERIP_BASE_ADDR (0x40000000UL) /* Defined peripheral base address for all peripheral */
 
 
 #endif /* INC_STM32F407XX_H_ */
