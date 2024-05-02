0d32a8c3b519da3786fe5cdfea737b61c13c9f52
Metehan Kemal Kayaalp <metehankemalkayaalp@gmail.com>
2024-05-02 21:50:25 +0300

    Update stm32f407xx.h
    APB1 Peripheral Base Addresses are defined

--- devdriver/Inc/stm32f407xx.h
+++ devdriver/Inc/stm32f407xx.h
@@ -13,21 +13,79 @@
  *
  */
 
-#define FLASH_BASE_ADDR  (0x08000000UL) /* Defined FLASH base address(up to 1 MB)  */
+#define FLASH_BASE_ADDR			(0x08000000UL)                          /* Defined FLASH base address(up to 1 MB) */
 
-#define SRAM1_BASE_ADDR  (0x20000000UL) /* Defined SRAM1 base address(up to 112 KB)*/
-#define SRAM2_BASE_ADDR  (0x2001C000UL) /* Defined SRAM2 base address(up to 16 KB) */
+#define SRAM1_BASE_ADDR			(0x20000000UL)                          /* Defined SRAM1 base address(up to 112 KB) */
+#define SRAM2_BASE_ADDR			(0x2001C000UL)                          /* Defined SRAM2 base address(up to 16 KB) */
 
 /*
  * Peripheral Base Addresses (Address starting hex number on reference manual)
  *
  */
 
-#define PERIP_BASE_ADDR (0x40000000UL)                 /* Defined peripheral base address for all peripheral */
+#define PERIP_BASE_ADDR			(0x40000000UL)                          /* Defined peripheral base address for all peripheral */
 
-#define APB1_BASE_ADDR  PERIP_BASE_ADDR                /*APB1 bus domain base address*/
-#define APB2_BASE_ADDR (PERIP_BASE_ADDR + 0x10000UL)   /*APB2 bus domain base address*/
-#define AHB1_BASE_ADDR (PERIP_BASE_ADDR + 0x20000UL)   /*AHB1 bus domain base address*/
-#define AHB2_BASE_ADDR (PERIP_BASE_ADDR + 0x10000000UL)/*AHB2 bus domain base address*/
+#define APB1_BASE_ADDR			(PERIP_BASE_ADDR + 0x00000000UL)        /* APB1 bus domain base address */
+#define APB2_BASE_ADDR			(PERIP_BASE_ADDR + 0x00010000UL)        /* APB2 bus domain base address */
+#define AHB1_BASE_ADDR			(PERIP_BASE_ADDR + 0x00020000UL)        /* AHB1 bus domain base address */
+#define AHB2_BASE_ADDR			(PERIP_BASE_ADDR + 0x10000000UL)        /* AHB2 bus domain base address */
+
+
+/*
+ * APB1 Peripheral Base Addresses (Address starting hex number on reference manual)
+ *
+ */
+
+#define TIM2_BASE_ADDR			(APB1_BASE_ADDR + 0x00000000UL)         /* Defined TIMER2 base address */
+#define TIM3_BASE_ADDR			(APB1_BASE_ADDR + 0x00000400UL)         /* Defined TIMER3 base address */
+#define TIM4_BASE_ADDR			(APB1_BASE_ADDR + 0x00000800UL)         /* Defined TIMER4 base address */
+#define TIM5_BASE_ADDR			(APB1_BASE_ADDR + 0x00000C00UL)         /* Defined TIMER5 base address */
+
+#define TIM6_BASE_ADDR			(APB1_BASE_ADDR + 0x00001000UL)         /* Defined TIMER6 base address */
+#define TIM7_BASE_ADDR			(APB1_BASE_ADDR + 0x00001400UL)         /* Defined TIMER7 base address */
+
+#define TIM12_BASE_ADDR			(APB1_BASE_ADDR + 0x00001800UL)         /* Defined TIMER12 base address */
+
+#define TIM13_BASE_ADDR			(APB1_BASE_ADDR + 0x00001C00UL)         /* Defined TIMER13 base address */
+#define TIM14_BASE_ADDR			(APB1_BASE_ADDR + 0x00002000UL)         /* Defined TIMER14 base address */
+
+#define RTC_BKP_REG_BASE_ADDR	(APB1_BASE_ADDR + 0x00002800UL)         /* Defined RTC & BKP Registers base address */
+
+#define WWDG_BASE_ADDR			(APB1_BASE_ADDR + 0x00002C00UL)         /* Defined WWDG base address */
+
+#define IWDG_BASE_ADDR			(APB1_BASE_ADDR + 0x00003000UL)         /* Defined IWDG base address */
+
+#define I2S2EX_BASE_ADDR		(APB1_BASE_ADDR + 0x00003400UL)         /* Defined I2S2ext base address */
+#define SPI2_BASE_ADDR			(APB1_BASE_ADDR + 0x00003800UL)         /* Defined SPI2 / I2S2 base address */
+#define SPI3_BASE_ADDR			(APB1_BASE_ADDR + 0x00003C00UL)         /* Defined SPI3 / I2S3 base address */
+#define I2S3EX_BASE_ADDR		(APB1_BASE_ADDR + 0x00004000UL)         /* Defined I2S3ext base address */
+
+#define USART2_BASE_ADDR		(APB1_BASE_ADDR + 0x00004400UL)         /* Defined USART2 base address */
+#define USART3_BASE_ADDR		(APB1_BASE_ADDR + 0x00004800UL)         /* Defined USART3 base address */
+#define UART4_BASE_ADDR			(APB1_BASE_ADDR + 0x00004C00UL)         /* Defined UART4 base address */
+#define UART5_BASE_ADDR			(APB1_BASE_ADDR + 0x00005000UL)         /* Defined UART5 base address */
+
+#define I2C1_BASE_ADDR			(APB1_BASE_ADDR + 0x00005400UL)			/* Defined I2C1 base address */
+#define I2C2_BASE_ADDR			(APB1_BASE_ADDR + 0x00005800UL)			/* Defined I2C2 base address */
+#define I2C3_BASE_ADDR			(APB1_BASE_ADDR + 0x00005C00UL)			/* Defined I2C3 base address */
+
+#define CAN1_BASE_ADDR			(APB1_BASE_ADDR + 0x00006400UL)			/* Defined CAN1 base address */
+#define CAN2_BASE_ADDR			(APB1_BASE_ADDR + 0x00006800UL)			/* Defined CAN2 base address */
+
+#define PWR_BASE_ADDR			(APB1_BASE_ADDR + 0x00007000UL)			/* Defined PWR base address */
+
+#define DAC_BASE_ADDR			(APB1_BASE_ADDR + 0x00007400UL)			/* Defined DAC base address */
+
+#define UART7_BASE_ADDR			(APB1_BASE_ADDR + 0x00007800UL)         /* Defined UART7 base address */
+#define UART8_BASE_ADDR			(APB1_BASE_ADDR + 0x00007C00UL)         /* Defined UART8 base address */
+
+/*
+ * APB2 Peripheral Base Addresses (Address starting hex number on reference manual)
+ *
+ */
+
+
+
+
 
 #endif /* INC_STM32F407XX_H_ */
