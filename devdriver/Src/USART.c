1f4b30ff405c5d412abc12b54d744571cff5e9f3
Metehan Kemal Kayaalp <metehankemalkayaalp@gmail.com>
2025-03-04 22:58:54 +0300

    USART Control Register2
    Writed Control Register2 configuration on USART.c file

--- devdriver/Src/USART.c
+++ devdriver/Src/USART.c
@@ -11,6 +11,15 @@ void USART_Init(USART_Handle_Typedef *USART_Handle){
 
 	uint32_t tempReg = 0;
 
+
+
+
+/*
+*
+* Control Register 1 Configuration
+*
+*/
+
 	tempReg = USART_Handle->Instance->USART_CR1;
 
 	tempReg |= (USART_Handle->Init.OverSampling)|(USART_Handle->Advance_Init.USART_EN)|(USART_Handle->Init.Wordlength)|\
@@ -18,5 +27,23 @@ void USART_Init(USART_Handle_Typedef *USART_Handle){
 			(USART_Handle->Advance_Init.RWU)|(USART_Handle->Advance_Init.SENDBRK);
 
 	USART_Handle->Instance->USART_CR1 = tempReg;
+
+
+
+
+/*
+*
+* Control Register 2 Configuration
+*
+*/
+
+	tempReg = USART_Handle->Instance->USART_CR2;
+
+	tempReg |= (USART_Handle->Advance_Init.LIN_MODE)|(USART_Handle->Init.StopBits)|(USART_Handle->Advance_Init.CLK_EN)|\
+			(USART_Handle->Advance_Init.CPOL)|(USART_Handle->Advance_Init.CPHA)|(USART_Handle->Advance_Init.LBCL);
+
+	USART_Handle->Instance->USART_CR2 = tempReg;
+
+
 }
 
