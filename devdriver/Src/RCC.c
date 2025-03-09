a11d3ed0433abae20422a0e1ba06fb8ac02bf46c
Metehan Kemal Kayaalp <metehankemalkayaalp@gmail.com>
2025-03-09 17:49:33 +0300

    Created uint32_t RCC_GetHClock(void)
    Created uint32_t RCC_GetHClock(void) function and prototype this function use for calculate AHB prescaler index

--- devdriver/Src/RCC.c
+++ devdriver/Src/RCC.c
@@ -10,7 +10,7 @@
 
 #include"RCC.h"
 
-
+const int8_t AHB_Prescaler[16]= {0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 6, 7, 8, 9};
 
 
 /*
@@ -52,8 +52,13 @@ uint32_t RCC_GetHClock(void){
 
 	uint32_t AHB_PeripClock = 0;
 	uint32_t SystemCoreClock = 0;
+	uint8_t HPRE_Value = 0;
 
 	SystemCoreClock = RCC_GetSystemClock();
+
+	HPRE_Value = ((RCC->CFGR >> 4U) & (0xFU));
+
+	AHB_PeripClock = (SystemCoreClock >> AHB_Prescaler[HPRE_Value]);
 
 
 	return AHB_PeripClock;
